ea65b032c58a94477b053868d1db23ec8bae9577
Hubert <hubert.zajac92@gmail.com>
2022-08-23 16:46:04 +0200

    Add routers paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AddTrainingView } from './views/AddTrainingView';
+import { EditTrainingView } from './views/EditTrainingView';
+import { LoginView } from './views/LoginView';
+import { NotFoundView } from './views/NotFoundView';
+import { SingupView } from './views/SignupView';
+import { SingleTrainingView } from './views/SingleTrainingView';
+import { TrainingsListView } from './views/TrainingsListView';
 
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      Start
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginView />} />
+      <Route path="/signup" element={<SingupView />} />
+      <Route path="/training" element={<TrainingsListView />} />
+      <Route path="/training/add-form" element={<AddTrainingView />} />
+      <Route path="/training/:title/:date" element={<SingleTrainingView />} />
+      <Route path="/training/edit-form/:title/:date" element={<EditTrainingView />} />
+      <Route path="*" element={<NotFoundView />} />
+    </Routes>
   );
 }
 
-export default App;
