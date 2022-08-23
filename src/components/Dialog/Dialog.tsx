831d223c96f90654bdca0d1d1947ccc62f315333
Hubert <hubert.zajac92@gmail.com>
2022-08-23 17:00:02 +0200

    Build to dialog to confirm delete records

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,41 @@
+import React, { Dispatch } from "react";
+
+import styles from './Dialog.module.scss';
+
+interface Props {
+    title: string;
+    date: Date | null;
+    setShowDialog: Dispatch<React.SetStateAction<boolean>>;
+    removeTraining: (title: string, date: Date) => Promise<void>
+}
+
+export const Dialog = ({ setShowDialog, removeTraining, title, date }: Props) => {
+
+    const confirmDelete = (title: string, date: Date) => {
+        removeTraining(title, date);
+        setShowDialog(false);
+    }
+
+
+    return (
+        <div className={styles.overlay} >
+            <div className={styles.dialog}>
+                <h2 className={styles.dialog__title}>Are you sure to delete thie training?</h2>
+                <button
+                    className={styles.dialog__btn}
+                    onClick={() => confirmDelete(title, date!)}
+                >
+                    Yes
+                </button>
+                <button
+                    className={styles.dialog__btn}
+                    onClick={() => setShowDialog(false)}
+                >
+                    No
+                </button>
+            </div >
+
+        </div >
+    )
+}
+
